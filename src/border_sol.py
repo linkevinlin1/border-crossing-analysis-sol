db820b1db6efbfcce5a1ff5113a4aa6f9a22b7e1
Kevin Lin <linkevinlin1@gmail.com>
2020-05-12 14:40:22 -0500

    try Atom

--- src/border_sol.py
+++ src/border_sol.py
@@ -25,26 +25,26 @@ from decimal import Decimal, ROUND_HALF_UP
 def column_init(column_list):
 
     #identify the indices for relevant columns
-    
+
     i=0
     for item in column_list:
         if item.lower() == 'border':
             Border_row_num=i
         elif item.lower() == 'date':
-            Date_row_num=i 
+            Date_row_num=i
         elif item.lower() == 'measure':
-            Measure_row_num=i 
+            Measure_row_num=i
         elif item.lower() == 'value':
-            Value_row_num=i 
+            Value_row_num=i
         i=i+1
     return [Border_row_num, Date_row_num, Measure_row_num, Value_row_num]
 
 def read_backwards(f,delimiter):
-    while f.read(1) != '\n':        
-        f.seek(f.tell() - 2, os.SEEK_SET) 
+    while f.read(1) != '\n':
+        f.seek(f.tell() - 2, os.SEEK_SET)
     current_line_num=f.tell()
     last_line = f.readline()
-    last_line=last_line.strip('\n')        
+    last_line=last_line.strip('\n')
     current_list = last_line.split(delimiter)
     current_list=[x.strip() for x in current_list]
     f.seek(current_line_num-4,0)
@@ -54,32 +54,32 @@ def reverse_csv():
 
     #reverse the line order of the temp file and write to report.csv
     f = open(str(sys.argv[2])+'.tmp','r')
-    
+
     first_line=f.readline()
     firstline_end= f.tell()
     f.seek(0, os.SEEK_END)
     f.seek(f.tell()-4, os.SEEK_SET)
-    
+
     newf = open(str(sys.argv[2]),'w')
     newf.write('Border,Date,Measure,Value,Average\n')
-    
+
     #read from the end
     while(f.tell()>firstline_end):
         while f.read(1) != '\n':
-            f.seek(f.tell() - 2, os.SEEK_SET) 
+            f.seek(f.tell() - 2, os.SEEK_SET)
         current_line_num=f.tell()
         last_line = f.readline()
         newf.write(last_line)
         f.seek(current_line_num-4,0)
-    newf.write(first_line)    
+    newf.write(first_line)
     newf.close()
     f.close()
-    
+
     #clean up the temp file
     os.remove(str(sys.argv[2])+'.tmp')
 
 def constuct_end_day(date):
-    
+
     #calculate the last time stamp of the current month range (12AM of the first day in the next month)
 
     #find first day of the current month
@@ -93,7 +93,7 @@ def constuct_end_day(date):
         return first_day
 
 def sort_and_output(stats_dic, home_country, month_num, last_end_day, fout):
-    #           (dictionary,name of country, month counter, last day of the time period, output file descriptor)  
+    #           (dictionary,name of country, month counter, last day of the time period, output file descriptor)
     end_day_str=time.strftime('%m/%d/%Y %I:%M:%S %p', last_end_day)
     #sort the ordered dictionary according to the output specification, key=(Border, Measure) and value of the dictionary=[Value, Running Sum]
     stats_dic=collections.OrderedDict(sorted(stats_dic.items(), key=lambda key_value_pair: (key_value_pair[1][0], key_value_pair[0][1], key_value_pair[0][0])))
@@ -113,14 +113,14 @@ def sort_and_output(stats_dic, home_country, month_num, last_end_day, fout):
 def check_key(stats_dic, current_list, column_index):
 
     #[Border_row_num, Date_row_num, Measure_row_num, Value_row_num]=column_index
-    
+
     border_name=current_list[column_index[0]].split()
     country_name=border_name[0].split('-')
-    
+
     #handle extra spaces in Measure name
     measure_list=current_list[column_index[2]].split()
     measure_name=' '.join(measure_list)
-    
+
     #key=(Border, Measure)
     dic_key=(country_name[-1].lower(), measure_name.lower())
 
@@ -169,19 +169,19 @@ def main():
     first_line=first_line.strip('\n')
     delimiter=check_delimiter(first_line)
     first_list=first_line.split(delimiter)
-    
+
     #remove extra spaces
     first_list=[x.strip() for x in first_list]
 
     #identify the relevant column indices
     column_index=column_init(first_list)
     [Border_row_num, Date_row_num, Measure_row_num, Value_row_num]=column_index
-    
+
 
     #read from the end, one line by one line
     firstline_end= f.tell()
     f.seek(0, os.SEEK_END)
-    f.seek(f.tell()-4, os.SEEK_SET) 
+    f.seek(f.tell()-4, os.SEEK_SET)
     while(f.tell()>firstline_end):
         current_list=read_backwards(f,delimiter)
 
@@ -194,21 +194,21 @@ def main():
         if first_entry_flag==1:
             last_end_day=constuct_end_day(Current_date)
             first_entry_flag=0
-            
+
         #check if the date of the current entry is in the same month range as the last one
         if constuct_end_day(Current_date)!=last_end_day:
-            
+
             #sort, calculate the running average, and output
             stats_dic=sort_and_output(stats_dic, home_country, month_num, last_end_day, fout)
-            month_num=month_num+1                
+            month_num=month_num+1
             last_end_day=constuct_end_day(Current_date)
 
         #check if the current entry matches the exisitng dictionary key and combine them. If not, create a new key fot it.
         stats_dic=check_key(stats_dic, current_list, column_index)
-            
+
     #exiting the loop, output the remaining entries in the last month range
     stats_dic=sort_and_output(stats_dic, home_country, month_num, last_end_day,fout)
-    
+
     f.close()
     fout.close()
 
